f35260ebb46f119ce15dbc82c1fbe7b06456de0a
Ren Aldridge <daviddarren220@gmail.com>
2024-11-28 13:08:30 +0800

    Update question.js

--- example/question.js
+++ example/question.js
@@ -1,9 +1,9 @@
 const Blackbox = require("../lib/blackbox");
-const nigg = new Blackbox();
+const ren = new Blackbox();
 
 async function askQuestion() {
   try {
-    let result = await nigg.question({ ask: "Keep it simple... What is blackbox?" });
+    let result = await ren.question({ ask: "Keep it simple... What is blackbox?" });
     console.log(result);
   } catch (error) {
     console.error("Error:", error.message);
